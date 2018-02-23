c8e5490cf41459259fd6c372aa8a44f9e95cb4a2
EIMIKI <kou.103.kan@gmail.com>
2018-02-24 08:27:39 +0900

    とりあえずテンプレートを追加

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	template *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.template = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.template.Execute(w, nil)
+}
+
+func main() {
+	//ルートへのハンドラー
+	http.Handle("/", &templateHandler{filename: "index.html"})
+
+	//サーバースタート
+	if err := http.ListenAndServe(":9000", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
