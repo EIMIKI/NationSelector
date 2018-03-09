5d62bc4ed239433f8a648b631f9867bc634792d6
EIMIKI <kou.103.kan@gmail.com>
2018-03-09 15:55:54 +0900

    選択項目が増加、結果も得られるようになった

--- main.go
+++ main.go
@@ -26,10 +26,19 @@ func main() {
 	http.Handle("/", &templateHandler{filename: "index.html"})
 	http.Handle("/select", &templateHandler{filename: "select.html"})
 	http.HandleFunc("/result", func(w http.ResponseWriter, r *http.Request) {
+
 		r.ParseForm()
+		v := r.Form
+
+		var req reqData
+		req.area = v.Get("area")
+		req.ideology = v.Get("ideology")
+		req.team = v.Get("team")
+		req.sea = v.Get("sea")
+		req.nf = v.Get("nf")
 
 		tmpl := template.Must(template.ParseFiles("./templates/result.html"))
-		tmpl.Execute(w, r.Form)
+		tmpl.Execute(w, selector(req))
 	})
 
 	//サーバースタート
---
+++ selector.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type reqData struct {
+	area, ideology, team, sea, nf string
+}
+
+func selector(req reqData) (nation string) {
+	db, err := sql.Open("mysql", "username:password@/nationsDb")
+	if err != nil {
+		log.Fatal("sql.Open:", err)
+	}
+	defer db.Close()
+
+	var sqlStr string = "select nation_name from nations where "
+	var flag bool = false
+	if req.area != "0" {
+		sqlStr += "area_id=" + req.area + " "
+		flag = true
+	}
+	if req.ideology != "0" {
+		if flag {
+			sqlStr = sqlStr + "and "
+		}
+		sqlStr += "ideology_id=" + req.ideology + " "
+		flag = true
+	}
+	if req.team != "0" {
+		if flag {
+			sqlStr = sqlStr + "and "
+		}
+		sqlStr += "team_id=" + req.team + " "
+		flag = true
+	}
+	if req.sea != "0" {
+		if flag {
+			sqlStr = sqlStr + "and "
+		}
+		sqlStr += "sea=" + req.sea + " "
+		flag = true
+	}
+	if req.nf != "0" {
+		if flag {
+			sqlStr = sqlStr + "and "
+		}
+		sqlStr += "nf=" + req.nf + " "
+		flag = true
+	}
+
+	if !flag {
+		sqlStr = "select nation_name from nations "
+	}
+	sqlStr += "order by rand() limit 1;"
+	fmt.Println(sqlStr)
+
+	err = db.QueryRow(sqlStr).Scan(&nation)
+	if err != nil {
+		if nation == "" {
+			nation = "該当なし"
+		} else {
+			log.Fatal("QueryRow:", err)
+		}
+	}
+	return
+}
